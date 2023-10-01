6cd0d36557959a9f018ad7716016bc405ed677ea
Feel <loneyfeel@gmail.com>
2023-10-01 19:39:18 +0300

    final commit

--- src/components/Main/Projects/Projects.js
+++ src/components/Main/Projects/Projects.js
@@ -1,187 +1,145 @@
 import './projects.css'
 import githubFillIcon from '../../../resources/icons/github-fill.svg'
 import viewIcon from '../../../resources/icons/view_icon.svg'
-import project_1 from '../../../resources/images/projects/project-1.jpg'
-import project_2 from '../../../resources/images/projects/project-2.jpg'
-import project_3 from '../../../resources/images/projects/project-3.jpg'
+
 import {motion} from "framer-motion";
 import {appCardAnimation} from '../../helpers/animation'
 import { useInView } from 'react-intersection-observer';
 
-const projectData = [
-    {
-        id:1,
-        img: project_1,
-        subTitle: "Website",
-        title:"Project 1",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "favorite",
-    },  {
-        id:2,
-        img: project_1,
-        subTitle: "Website",
-        title:"Project 1",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "favorite",
-    },  {
-        id:3,
-        img: project_1,
-        subTitle: "Website",
-        title:"Project 1",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "favorite",
-    }, {
-        id:4,
-        img: project_1,
-        subTitle: "Website",
-        title:"Project 1",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwwdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "favorite",
-    },{
-        id:5,
-        img: project_1,
-        subTitle: "Website",
-        title:"Project 1",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwwdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "favorite",
-    }, {
-        id:6,
-        img: project_2,
-        subTitle: "Website",
-        title:"Project Project",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwdwwasdwasdwasdwasdwv",
-        gitLink: "",
-        viewLink: "",
-        status: "educational",
-    },{
-        id:7,
-        img: project_2,
-        subTitle: "Website",
-        title:"Project Project",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwdwwasdwasdwasdwasdwv",
-        gitLink: "",
-        viewLink: "",
-        status: "educational",
-    }, {
-        id:8,
-        img: project_3,
-        subTitle: "Website",
-        title:"Bee BeeEEE BEEEEEEE",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "in progress",
-    },{
-        id:9,
-        img: project_3,
-        subTitle: "Website",
-        title:"Bee BeeEEE BEEEEEEE",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "in progress",
-    },{
-        id:10,
-        img: project_3,
-        subTitle: "Website",
-        title:"Bee BeeEEE BEEEEEEE",
-        description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
-        gitLink: "",
-        viewLink: "",
-        status: "in progress",
-    },
-]
+import {projectData} from './projectData'
+
+// const projectData = [
+//     {
+//         id:1,
+//         img: project_1,
+//         subTitle: "Website",
+//         title:"Project 1",
+//         description: "",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "favorite",
+//     },  {
+//         id:2,
+//         img: project_1,
+//         subTitle: "Website",
+//         title:"Project 1",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "favorite",
+//     },  {
+//         id:3,
+//         img: project_1,
+//         subTitle: "Website",
+//         title:"Project 1",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwsdwwasdwasdwasdwasdwwasdwasdwasdwasdwwdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "favorite",
+//     }, {
+//         id:4,
+//         img: project_1,
+//         subTitle: "Website",
+//         title:"Project 1",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwwdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "favorite",
+//     },{
+//         id:5,
+//         img: project_1,
+//         subTitle: "Website",
+//         title:"Project 1",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwasdwasdwwdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "favorite",
+//     }, {
+//         id:6,
+//         img: project_2,
+//         subTitle: "Website",
+//         title:"Project Project",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwdwwasdwasdwasdwasdwv",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "educational",
+//     },{
+//         id:7,
+//         img: project_2,
+//         subTitle: "Website",
+//         title:"Project Project",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdwwasdwdwwasdwasdwasdwasdwv",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "educational",
+//     }, {
+//         id:8,
+//         img: project_3,
+//         subTitle: "Website",
+//         title:"Bee BeeEEE BEEEEEEE",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "in progress",
+//     },{
+//         id:9,
+//         img: project_3,
+//         subTitle: "Website",
+//         title:"Bee BeeEEE BEEEEEEE",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "in progress",
+//     },{
+//         id:10,
+//         img: project_3,
+//         subTitle: "Website",
+//         title:"Bee BeeEEE BEEEEEEE",
+//         description: "wasdwasdwasdwasdwwasdwasdwasdwasdwwasdwasdwasdwasdw",
+//         gitLink: "",
+//         viewLink: "",
+//         status: "in progress",
+//     },
+// ]
 function Projects() {
     return (
         <>
             <section className="projects section" id="projects">
                 <div className="title section__title-1"><p>Projects</p></div>
                 <div className="project__container container">
-                    <div className="title projects__title"><p>Favorites</p></div>
-                    <div className="projects__cards">
-                        {projectData
-                            .filter((cardData) => cardData.status === "favorite")
-                            .map((filteredData,index) => (
-                                <motion.div
-                                    variants={appCardAnimation}
-                                    initial="initial"
-                                    whileInView="animate"
-                                    viewport={{
-                                        once: true,
-                                    }}
-                                    custom={index}>
-                                    <ProjectCard
-                                    key={filteredData.id}
-                                    projectImg={filteredData.img}
-                                    projectSubTitle={filteredData.subTitle}
-                                    projectTitle={filteredData.title}
-                                    projectDecr={filteredData.description}
-                                    projectGit={filteredData.gitLink}
-                                    projectView={filteredData.viewLink}
-                                />
-                                </motion.div>
-                            ))}
-                    </div>
-                    <div className="title projects__title"><p>Educational</p></div>
-                    <div className="projects__cards">
-                        {projectData
-                            .filter((cardData) => cardData.status === "educational")
-                            .map((filteredData, index) => (
-                                <motion.div
-                                    variants={appCardAnimation}
-                                    initial="initial"
-                                    whileInView="animate"
-                                    viewport={{
-                                        once: true,
-                                    }}
-                                    custom={index}>
-                                <ProjectCard
-                                    key={filteredData.id}
-                                    projectImg={filteredData.img}
-                                    projectSubTitle={filteredData.subTitle}
-                                    projectTitle={filteredData.title}
-                                    projectDecr={filteredData.description}
-                                    projectGit={filteredData.gitLink}
-                                    projectView={filteredData.viewLink}
-                                />
-                                </motion.div>
-                            ))}
-                    </div>
-                    <div className="title projects__title"><p>In progress</p></div>
-                    <div className="projects__cards">
-                        {projectData
-                            .filter((cardData) => cardData.status === "in progress")
-                            .map((filteredData, index) => (
-                            <motion.div
-                            variants={appCardAnimation}
-                            initial="initial"
-                            whileInView="animate"
-                            viewport={{
-                            once: true,
-                        }}
-                            custom={index}>
-                                <ProjectCard
-                                    key={filteredData.id}
-                                    projectImg={filteredData.img}
-                                    projectSubTitle={filteredData.subTitle}
-                                    projectTitle={filteredData.title}
-                                    projectDecr={filteredData.description}
-                                    projectGit={filteredData.gitLink}
-                                    projectView={filteredData.viewLink}
-                                />
-                            </motion.div>
-                            ))}
-                    </div>
+                    {['favorite', 'educational', 'in progress'].map((status) => (
+                        <div key={status}>
+                            <div className="title projects__title">
+                                <p>{status === 'favorite' ? 'Favorites' : status === 'educational' ? 'Educational' : 'In progress'}</p>
+                            </div>
+                            <div className="projects__cards">
+                                {projectData
+                                    .filter((cardData) => cardData.status === status)
+                                    .reverse()
+                                    .map((filteredData, index) => (
+                                        <motion.div
+                                            variants={appCardAnimation}
+                                            initial="initial"
+                                            whileInView="animate"
+                                            viewport={{
+                                                once: true,
+                                            }}
+                                            custom={index}
+                                            key={filteredData.id}
+                                        >
+                                            <ProjectCard
+                                                projectImg={filteredData.img}
+                                                projectSubTitle={filteredData.subTitle}
+                                                projectTitle={filteredData.title}
+                                                projectDecr={filteredData.description}
+                                                projectGit={filteredData.gitLink}
+                                                projectView={filteredData.viewLink}
+                                            />
+                                        </motion.div>
+                                    ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </section>
         </>
@@ -190,10 +148,10 @@ function Projects() {
 export default Projects
 
 function ProjectCard({projectImg,projectSubTitle,projectTitle,projectDecr,projectGit,projectView}) {
-    // Используем хук useInView для отслеживания видимости элемента
+    // хук useInView для отслеживания видимости элемента
     const [ref, inView] = useInView({
         triggerOnce: false, // Чтобы событие сработало только один раз
-        threshold: 0.25, // Задаем порог видимости (от 0 до 1)
+        threshold: 0.4, // Порог видимости (от 0 до 1)
     });
     const bgcProjectClass = inView ? 'projects__card-active' : 'projects__card-inactive';
     return (
---
+++ src/components/Main/Projects/projectData.js
@@ -0,0 +1,74 @@
+import project_uber_img from "../../../resources/images/projects/project_uber_img.png";
+import project_pulse_img from "../../../resources/images/projects/project_pulse_img.png";
+import project_coffeeShop_img from "../../../resources/images/projects/project_coffeeShop_img.png";
+import project_eduweb_img from "../../../resources/images/projects/project_eduweb_img.png";
+import project_volti_img from "../../../resources/images/projects/project_volti_img.png";
+import project_aboutMe_img from "../../../resources/images/projects/project_aboutMe_img.png";
+import project_portfolio_img from "../../../resources/images/projects/project_portfolio_img.png";
+
+export const projectData = [
+    {
+        id:1,
+        img: project_coffeeShop_img,
+        subTitle: "Site layout",
+        title:"Coffee Shop",
+        description: "Easy coffee shop website with adaptability",
+        gitLink: "https://github.com/Loneyfeel/educational-projects/tree/main/coffeeShop",
+        viewLink: "https://loneyfeel.github.io/educational-projects/coffeeShop/",
+        status: "educational",
+    },  {
+        id:2,
+        img: project_pulse_img,
+        subTitle: "Site layout",
+        title:"Heart rate monitor magazine",
+        description: "Website-shop for the selection and purchase of the correct heart rate monitor",
+        gitLink: "https://github.com/Loneyfeel/educational-projects/tree/main/pulse",
+        viewLink: "https://loneyfeel.github.io/educational-projects/pulse/",
+        status: "educational",
+    },  {
+        id:3,
+        img: project_uber_img,
+        subTitle: "Site layout",
+        title:"Uber",
+        description: "The site offers drivers to become Uber partners.",
+        gitLink: "https://github.com/Loneyfeel/educational-projects/tree/main/uber",
+        viewLink: "https://loneyfeel.github.io/educational-projects/uber/",
+        status: "educational",
+    }, {
+        id:4,
+        img: project_eduweb_img,
+        subTitle: "Site layout",
+        title:"EduWeb",
+        description: "An online school for teaching various professions with the opportunity to enroll in a course or buy one.",
+        gitLink: "https://github.com/Loneyfeel/educational-projects/tree/main/eduweb",
+        viewLink: "https://loneyfeel.github.io/educational-projects/eduweb/",
+        status: "favorite",
+    },{
+        id:5,
+        img: project_volti_img,
+        subTitle: "Site layout",
+        title:"Volti",
+        description: "This website offers services for the purchase of electric scooters. charging stations, etc.",
+        gitLink: "https://github.com/Loneyfeel/educational-projects/tree/main/volti",
+        viewLink: "https://loneyfeel.github.io/educational-projects/volti/",
+        status: "favorite",
+    }, {
+        id:6,
+        img: project_aboutMe_img,
+        subTitle: "Website",
+        title:"About me",
+        description: "About me - is a website rethinking the creation and distribution of your resumes, portfolios or business cards.",
+        gitLink: "https://github.com/Loneyfeel/about_me",
+        viewLink: "https://about-me-app-94619db01393.herokuapp.com/",
+        status: "in progress",
+    },{
+        id:7,
+        img: project_portfolio_img,
+        subTitle: "Website",
+        title:"My portfolio-website",
+        description: "The site you are on was created as part of improving React skills and creating an adaptive design with a dark theme.",
+        gitLink: "https://github.com/Loneyfeel/portfolio",
+        viewLink: "https://loneyfeel.github.io/portfolio/",
+        status: "favorite",
+    },
+]
